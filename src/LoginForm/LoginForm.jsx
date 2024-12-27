39ba728f70782c54f110d07342a58d23fd9ea378
Soumitra-Mandal19 <mandal.soumitra@outlook.com>
2024-12-27 23:19:21 +0530

    Changed label syntax

--- src/LoginForm/LoginForm.jsx
+++ src/LoginForm/LoginForm.jsx
@@ -11,7 +11,7 @@ export const LoginForm = () => {
         e.preventDefault();
         if (username === "user" && password === "password" ){
             setIsLoggedIn(true);
-            setMessage(`Welcome, ${username}!`)
+            setMessage(`Welcome, user!`)
             setUserName("");
             setPassword("");
         } else {
@@ -27,16 +27,16 @@ export const LoginForm = () => {
             <h1>Login Page</h1>
             <div className={styles.container}>
             {!isLoggedIn ? <div>{message}</div> : null}
-          {!isLoggedIn ?  <form action="" onSubmit={handleSubmit}>
+          {!isLoggedIn ?  <form action="/submit-form" onSubmit={handleSubmit}>
                 <div>
-                    <label>Username:
+                    <label for='username'>Username</label>
                         <input required value={username} onChange={(e)=> setUserName(e.target.value)} type="text" placeholder="username"/>
-                    </label>
+                    
                 </div>
                 <div>
-                    <label>Password:
+                    <label for='password'>Password</label>
                         <input required value={password} onChange={(e)=> setPassword(e.target.value)} type="password" placeholder="password"/>
-                    </label>
+                    
                 </div>
                 <button  type="submit">Submit</button>
             </form> : null}
